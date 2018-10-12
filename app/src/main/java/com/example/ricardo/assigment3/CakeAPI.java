3adc1a4bfc90905eae3285864cd7168b4991c038
Rick <ricardolira48@hotmail.com>
2018-10-12 11:47:57 -0400

    made it asyncronous

--- app/src/main/java/com/example/ricardo/assigment3/CakeAPI.java
+++ app/src/main/java/com/example/ricardo/assigment3/CakeAPI.java
@@ -1,14 +1,16 @@
 package com.example.ricardo.assigment3;
 
 import java.util.List;
+import io.reactivex.Observable;
 
 import retrofit2.Call;
+
 import retrofit2.http.GET;
 
 public interface CakeAPI {
 
     String BASE_URL = "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/";
     @GET("8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json")
-    Call<List<Cake>> getCakes();
+    Observable<List<Cake>> getCakes();
 
 }
--- app/src/main/java/com/example/ricardo/assigment3/MainActivity.java
+++ app/src/main/java/com/example/ricardo/assigment3/MainActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,39 +26,39 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recycler;
+    CompositeDisposable mCom;
     ArrayList<Cake> datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mCom = new CompositeDisposable();
         recycler = (RecyclerView) findViewById(R.id.recycler_cake);
         recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
         datos = new ArrayList<Cake>();
-        Retrofit retrofit = new Retrofit.Builder()
+        CakeAPI retrofit = new Retrofit.Builder()
                 .baseUrl(CakeAPI.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        CakeAPI api = retrofit.create(CakeAPI.class);
-        Call<List<Cake>> call = api.getCakes();
-        call.enqueue(new Callback<List<Cake>>() {
+                .build().create(CakeAPI.class);
+        mCom.add(retrofit.getCakes()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse,this::handleError));
 
-            @Override
-            public void onResponse(Call<List<Cake>> call, Response<List<Cake>> response) {
-                List<Cake> cakes = response.body();
-                System.out.println("hello");
-                /*for(int i=0;i<=cakes.size();i++){
-                    datos.add(new Cake(cakes.get(i).getTitle());
-                }*/
-                AdapterCake adapter = new AdapterCake(cakes);
-                recycler.setAdapter(adapter);
-            }
 
-            @Override
-            public void onFailure(Call<List<Cake>> call, Throwable t) {
-                System.out.println("error ------> "+t.getMessage());
-            }
-        });
+    }
+    private void handleResponse(List<Cake> cake){
 
+        datos = new ArrayList<>(cake);
+        AdapterCake adapter = new AdapterCake(datos);
+        recycler.setAdapter(adapter);
+        
+
+    }
+
+    private void handleError(Throwable error) {
+
+        Toast.makeText(this, "Error "+error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
     }
 }
